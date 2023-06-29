b078ad5d8c8e09ded1dd14c8abef44f1b51d8965
gustavof04 <fernandes.gustavo2910@gmail.com>
2023-06-29 14:26:26 -0400

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// Testando o hamburger menu (em desenvolvimento)
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+// Ao rolar a tela até atingir a <div> artworks, o background muda.
+window.addEventListener('scroll', function() {
+    let artworksDiv = document.getElementById('artworks');
+
+    if (artworksDiv) {
+        let artworksPosition = artworksDiv.getBoundingClientRect().top;
+        let scrollPosition = window.innerHeight;
+        let marginPercentage = 99;
+        let margin = (scrollPosition * marginPercentage) / 100;
+
+        if (artworksPosition < (scrollPosition - margin)) {
+            this.document.body.classList.add('scrolled', 'fade-in')
+        } else {
+            this.document.body.classList.remove('scrolled')
+        }
+    }
+});
